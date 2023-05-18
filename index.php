25f6ffc9fe2b1117f2b9964e52397cc22fc8744f
Rehan KU <zrehan286@gmail.com>
2023-05-18 15:55:45 +0530

    Solved the problem

--- index.php
+++ index.php
@@ -1,23 +1,32 @@
 <?php
 // timestamp: 3:00
-$array = [5, 2, 1];
-//$array = [4, 1, 2];
-
+//$array = [5, 2, 1];
+//$array = [1, 2, 5];
+$array = [4, 1, 2, 5];
+$result = [];
 if (isDescendingOrder($array)) {
     echo "The array is already arranged in descending order.";
 } else {
-    // TODO
-    echo "WIP";
+    do {
+        for ($i = 0; $i < count($array) - 1; $i++) {
+            if ($array[$i] < $array[$i + 1]) {
+                $temp = $array[$i + 1];
+                $array[$i + 1] = $array[$i];
+                $array[$i] = $temp;
+                // $result[] = $array;
+                //
+                array_push($result, $array);
+            }
+        }
+    } while (! isDescendingOrder($array));
+    print_r($result);
 }
 function isDescendingOrder($array)
 {
-    // largest
-    // comparison
     for ($i = 0; $i < (count($array) - 1); $i++) {
         if ($array[$i] < $array[$i + 1]) {
             return false;
         }
     }
-
     return true;
 }
